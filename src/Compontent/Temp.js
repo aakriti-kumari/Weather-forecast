ee60bd8d79b4d3d15d03cd06d4070b3c98994e07
Akriti kumari <124522357+aakriti-kumari@users.noreply.github.com>
2023-11-24 01:28:00 +0530

    Add files via upload

---
+++ src/Compontent/Temp.js
@@ -0,0 +1,13 @@
+import React from  "react";
+import "./css/style.css";
+
+function App(){
+    return(
+        <>
+          <div className="box">
+
+          </div>
+        </>
+    )
+}
+export default App;
